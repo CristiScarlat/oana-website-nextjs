b58666ad88e416fb39754c67b965eea8981d326b
scarlat-trainer <78.cristi@gmail.com>
2024-06-11 23:00:29 +0300

    add media locally

--- pages/media.js
+++ pages/media.js
@@ -6,10 +6,10 @@ const Media = () => {
             <h1 className="p-2">Media</h1>
             <div className="video-container p-2">
                 <video width="400" controls>
-                    <source src={process.env.NEXT_PUBLIC_MEDIA_1_URL} type="video/mp4" />
+                    <source src="videos/hypnotic.mp4" type="video/mp4" />
                 </video>
                 <video width="400" controls>
-                    <source src={process.env.NEXT_PUBLIC_MEDIA_2_URL} type="video/mp4" />
+                    <source src="videos/interview.mp4" type="video/mp4" />
                 </video>
             </div>
         </>
