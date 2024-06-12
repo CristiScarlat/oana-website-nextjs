89b7f5774508af306aa3a6dffdad3dffa772be94
scarlat-trainer <78.cristi@gmail.com>
2024-06-12 12:55:21 +0300

    update github workflows

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/oanabologbleich.com",
-  assetPrefix: "/oanabologbleich.com"
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
